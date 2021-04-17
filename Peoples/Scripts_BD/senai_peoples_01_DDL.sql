e97a807360ead57de22aa63ce54ccf0d2f79af2d
Caique Santos <caisbrito2003@gmail.com>
2021-04-17 19:05:03 -0300

    Banco de dados add

---
+++ Peoples/Scripts_BD/senai_peoples_01_DDL.sql
@@ -0,0 +1,15 @@
+-- DDL
+CREATE DATABASE Peoples;
+
+USE Peoples;
+
+CREATE TABLE Funcionarios 
+(
+	IdFuncionario	INT IDENTITY PRIMARY KEY
+	,Nome			VARCHAR(200) NOT NULL
+	,Sobrenome		VARCHAR(255)
+);
+GO
+
+ALTER TABLE Funcionarios
+ADD DataNascimento DATE
---
+++ Peoples/Scripts_BD/senai_peoples_02_DML.sql
@@ -0,0 +1,8 @@
+-- DML
+USE Peoples;
+
+INSERT INTO Funcionarios	(Nome, Sobrenome) 
+VALUES						('Catarina', 'Strada')
+							,('Tadeu', 'Vitelli');
+
+UPDATE Funcionarios SET DataNascimento = '1993-03-17';
